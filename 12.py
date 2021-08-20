05bd8b875d78cb60e7108fb72fce52106242ced3
olegomario <marioolego@gmail.com>
2021-08-20 12:27:26 +0200

    Solved problem 12

--- 12.py
+++ 12.py
@@ -1,15 +1,22 @@
-def eulerSum(n):
+import math
+
+def eulerSum(n): #Euler's formula to find the total sum of all integers up to an integer n
     eulerSum=int(n*(n+1)/2)
     return eulerSum
 
-def factorCounter(n):
-    counter=1 #Count as factor the number itself
-    for i in range(1,n):
-        if n%i==0:
-            counter+=1
-    return counter
+def get_factors(n):
+    sum=0
+    for i in range(1, math.ceil(math.sqrt(n))): #All unique factors are smaller than sqrt(n)
+        if n % i == 0:
+            sum+=2 #Count also the duplicate factor
+    return sum
 
-#while factorCounter(eulerSum(number))<500:
-   #number+=1
+i=1
+factorLimit=500
 
-print(factorCounter(eulerSum(7)))
+while get_factors((eulerSum(i)))<factorLimit:
+    i+=1
+
+print(eulerSum(i))
+
+
